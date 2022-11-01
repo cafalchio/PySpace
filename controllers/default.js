b0d15bc3916851c2dadbb28c27949cf8eb0c9e54
cafalchio <maolivei@tcd.ie>
2022-11-01 23:35:45 +0000

    reset terminal size

--- controllers/default.js
+++ controllers/default.js
@@ -18,7 +18,7 @@ function socket() {
         // Spawn terminal
         client.tty = Pty.spawn('python3', ['run.py'], {
             name: 'xterm-color',
-            cols: 150,
+            cols: 120,
             rows: 40,
             cwd: process.env.PWD,
             env: process.env
